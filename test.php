94f90c7d85758c76865f1a44ed8b0002af825d12
aun nuseir <nuseiraun@icloud.com>
2020-12-14 12:28:02 +0000

    css

--- test.php
+++ test.php
@@ -1,3 +1,26 @@
 <?php
-echo gethostname();
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="website.css">
+</head>
+
+<div class="header">
+  <h1>My Website</h1>
+  <p>Resize the browser window to see the effect.</p>
+</div>
+
+<div class="topnav">
+  <a href="#" style="float:center">Homepage</a>
+</div>
+
+<div class="footer">
+  <h2>Open weahtermap</h2>
+  <h2> Net302 </h2>
+</div>
+
+</body>
+</html>
