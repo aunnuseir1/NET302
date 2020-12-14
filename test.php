8e214579b6f5f8a22bf8aaa2026b21c1cc6f7fa1
aun nuseir <nuseiraun@icloud.com>
2020-12-14 14:40:20 +0000

    indent

--- test.php
+++ test.php
@@ -4,23 +4,23 @@
 
 <html>
 <head>
-<link rel="stylesheet" href=".. /net302/webiste.css" type="text/css">
+  <link rel="stylesheet" href=".. /net302/webiste.css" type="text/css">
 </head>
 
 <body>
-<div class="header">
-  <h1>My Website</h1>
-  <p>Resize the browser window to see the effect.</p>
-</div>
+  <div class="header">
+    <h1>My Website</h1>
+    <p>Resize the browser window to see the effect.</p>
+  </div>
 
-<div class="topnav">
-  <a href="#" style="float:center">Homepage</a>
-</div>
+  <div class="topnav">
+    <a href="#" style="float:center">Homepage</a>
+  </div>
 
-<div class="footer">
-  <h2>Open weahtermap</h2>
-  <h2> Net302 </h2>
-</div>
+  <div class="footer">
+    <h2>Open weahtermap</h2>
+    <h2> Net302 </h2>
+  </div>
 
 </body>
 </html>
